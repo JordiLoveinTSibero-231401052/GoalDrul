b8a0dfa158204be02fdba5762b7ddb570dab9b2f
JordiLoveinTSibero-231401052 <151598727+JordiLoveinTSibero-231401052@users.noreply.github.com>
2024-11-15 19:51:16 +0700

    Update dashboard.php

--- gdbaru/dashboard.php
+++ gdbaru/dashboard.php
@@ -104,7 +104,7 @@ function fetch_fixtures($league_id, $api_key, $ch, $fixtures_url, $today) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="newcss/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
